a88ff5b35cd41e1625565f13ea5e91a46de4682d
Berkan Kocek <berkankocek1@gmail.com>
2026-04-20 14:01:52 +0300

    fix: redirect old cup category

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/promosyon-seramik-porselen-kupalar',
+        destination: '/promosyon-kupa',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
